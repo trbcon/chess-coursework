5222e3f7bc1ea1e7e5b9b3e92d521140fbc1916c
trbcon <pahax.off@gmail.com>
2026-04-01 18:26:31 +0300

    frist commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char white_figures = {"♔", "♕", "♖", "♗", "♘", "♙"} 
+	char black_figures = {"♚", "♛", "♜", "♝", "♞", "♟"} 
+	cout << "♔";
+
+    return 0;
+}
